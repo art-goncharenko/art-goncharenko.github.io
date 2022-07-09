19397b4997b3d5360d820f7bec7b19dff42e6237
art-goncharenko <98996194+art-goncharenko@users.noreply.github.com>
2022-07-09 16:55:59 -0400

    Add files via upload
    init

---
+++ precache-manifest.6132a2f3e39f809cf0f864d4481059ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f99031855666f05195d11433a53d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e7af2f3d4be5a58c90",
+    "url": "/static/css/main.7de23c5f.chunk.css"
+  },
+  {
+    "revision": "cc81ab5b8e5ab5acd51f",
+    "url": "/static/js/2.e7fc1f7b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e7fc1f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e7af2f3d4be5a58c90",
+    "url": "/static/js/main.d40abd28.chunk.js"
+  },
+  {
+    "revision": "b8c053cb2522392dd525",
+    "url": "/static/js/runtime-main.d9469fe2.js"
+  }
+]);
